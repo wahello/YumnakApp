2ab20cb96e663c6e7225a3e0d0e5f65ee735a3f1
gpgirls20 <gpgirls20@gmail.com>
2020-02-09 13:53:28 +0300

    Shahad's Sun 2:00

--- lib/models/Customer.dart
+++ lib/models/Customer.dart
@@ -1,24 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:yumnak/models/user.dart';
 
-class Customer{
-
-  //List<CustomerRate> _rateArray;
-  //double _avgRate;
-
-  //Customer(String phoneNumber, String name, String email, String password, var location, String userType, this._rateArray, this._avgRate)
-    //  :super(phoneNumber, name, email, password, location, userType);
-
-  /*set rateArray(List<CustomerRate> rate){
-    location=rate;
-  }
-  
-  set avgRate(double avg){
-    avgRate=avg;
-  }
-
-  List<CustomerRate> get rateArray => rateArray;
-  double get avgRate => avgRate;*/
+class Customer extends User{
 
 
   String name;
--- lib/models/user.dart
+++ lib/models/user.dart
@@ -5,38 +5,5 @@ class User {
 
   User({this.uid});
 
-  String _phoneNumber;
-  String _name;
-  String _email;
-  String _password;
-  var _location;
-  String _userType;
 
-  //User(this._phoneNumber, this._name, this._email, this._password, this._location, this._userType);
-
-  set phoneNumber(String pNum){
-    phoneNumber=pNum;
-  }
-  set name(String n){
-    name=n;
-  }
-  set email(String e){
-    email=e;
-  }
-  set password(String p){
-    password=p;
-  }
-  set location(var l){
-    location=l;
-  }
-  set userType(String uType){
-    userType=uType;
-  }
-
-  String get phoneNumber => phoneNumber;
-  String get name => name;
-  String get email => email;
-  String get password => password;
-  //var get location => location;
-  String get userType => userType;
 }
