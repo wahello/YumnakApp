0a857d42e386d6cdd1c7f9e0cc1f896df53de077
gpgirls20 <gpgirls20@gmail.com>
2020-02-10 21:18:11 +0300

    shahad and maha commite 9:18

--- lib/screens/Main.dart
+++ lib/screens/Main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:yumnak/screens/SignIn_Cust.dart';
 import 'package:yumnak/screens/SignIn_SP.dart';
-import 'package:yumnak/screens/wrapper.dart';
 
 
 class Main extends StatefulWidget {
--- lib/screens/SignIn_SP.dart
+++ lib/screens/SignIn_SP.dart
@@ -25,148 +25,148 @@ class _SignIn_SPState extends State<SignIn_SP> {
     return Scaffold(
         resizeToAvoidBottomPadding: false,
         body: Container(
-          child: Form(
-              key: _formKey,  //for validation
-              child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: <
-                  Widget>[
-                Container(
-                  child: Stack(
-                    children: <Widget>[
-                      Container(
-                        padding: EdgeInsets.fromLTRB(11.0, 40.0, 0.0, 30.0),
-                        child: Text(
-                          'تسجيل دخول',
-                          style:
-                          TextStyle(color: Colors.lightBlueAccent, fontSize: 40.0, fontWeight: FontWeight.bold, fontFamily: "Montserrat"),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-
-                Container(
-                  padding: EdgeInsets.fromLTRB(160.0, 00.0, 0.0, 0.0),
-                  child: Text(
-                    '..أهلا بعودتك',
-                    style:
-                    TextStyle(color: Colors.grey[600], fontSize: 20.0, fontWeight: FontWeight.bold, fontFamily: "Montserrat"),
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.fromLTRB(90.0, 00.0, 0.0, 0.0),
-                  child: Text(
-                    'فضلاً أدخل معلومات الدخول',
-                    style:
-                    TextStyle(color: Colors.grey[600], fontSize: 15.0, fontFamily: "Montserrat"),
-                  ),
-                ),
-
-                Container(
-                    padding: EdgeInsets.only(top: 20.0, left: 20.0, right: 20.0),
-                    child: Column(
+            child: Form(
+                key: _formKey,  //for validation
+                child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: <
+                    Widget>[
+                  Container(
+                    child: Stack(
                       children: <Widget>[
-                        Directionality(
-                            textDirection: TextDirection.rtl,
-                            child:TextFormField(
-                              validator: (val) => val.isEmpty ? "Enter an email" : null,  //null means valid email
-                              decoration: InputDecoration(
-                                  labelText:  'البريد الإلكتروني',
-                                  labelStyle: TextStyle( fontFamily: 'Montserrat',fontWeight: FontWeight.bold, color: Colors.grey),
-                                  focusedBorder: UnderlineInputBorder(
-                                  borderSide: BorderSide(color: Colors.lightBlueAccent))),
-                                  onChanged: (val){setState(() => email=val);},
-                            )
-                        ),
-                        SizedBox(height: 10.0),
-                        Directionality(
-                            textDirection: TextDirection.rtl,
-                            child:TextFormField(
-                              validator: (val) => val.length < 6 ? "enter a password 6+ chars long" : null,  //null means valid password
-                              decoration: InputDecoration(
-                                  labelText: 'كلمة المرور ',
-                                  labelStyle: TextStyle(fontFamily: 'Montserrat',fontWeight: FontWeight.bold,color: Colors.grey),
-                                  focusedBorder: UnderlineInputBorder(
-                                      borderSide: BorderSide(color: Colors.lightBlueAccent))),
-                              obscureText: true,
-                              onChanged: (val){ setState(() => password =val);},
-                            )
-                        ),
-                        SizedBox(height: 10.0),
-
                         Container(
-                          padding: EdgeInsets.fromLTRB(0.0, 00.0, 200.0, 50.0),
-                          child: GestureDetector(
-                              child: Text('نسيت كلمة المرور ؟', style:
-                              TextStyle(color: Colors.grey[600], fontSize: 15.0, fontFamily: "Montserrat"),),
-                              onTap: () {
-                                Navigator.push(context, new MaterialPageRoute(
-                                    builder: (context) => ForgetPassword()
-                                ));
-                              }
-
+                          padding: EdgeInsets.fromLTRB(11.0, 40.0, 0.0, 30.0),
+                          child: Text(
+                            'تسجيل دخول',
+                            style:
+                            TextStyle(color: Colors.lightBlueAccent, fontSize: 40.0, fontWeight: FontWeight.bold, fontFamily: "Montserrat"),
                           ),
                         ),
+                      ],
+                    ),
+                  ),
 
-                        Container(
-                            height: 40.0,
-                            child: Material(
-                              borderRadius: BorderRadius.circular(20.0),
-                              shadowColor: Colors.lightBlueAccent,
-                              color: Colors.green[300],
-                              elevation: 7.0,
-                              child: GestureDetector(
+                  Container(
+                    padding: EdgeInsets.fromLTRB(160.0, 00.0, 0.0, 0.0),
+                    child: Text(
+                      '..أهلا بعودتك',
+                      style:
+                      TextStyle(color: Colors.grey[600], fontSize: 20.0, fontWeight: FontWeight.bold, fontFamily: "Montserrat"),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.fromLTRB(90.0, 00.0, 0.0, 0.0),
+                    child: Text(
+                      'فضلاً أدخل معلومات الدخول',
+                      style:
+                      TextStyle(color: Colors.grey[600], fontSize: 15.0, fontFamily: "Montserrat"),
+                    ),
+                  ),
 
-                                //اذا دخل ايميل وباسورد مو موجودات مايطلع له مسج !!
+                  Container(
+                      padding: EdgeInsets.only(top: 20.0, left: 20.0, right: 20.0),
+                      child: Column(
+                        children: <Widget>[
+                          Directionality(
+                              textDirection: TextDirection.rtl,
+                              child:TextFormField(
+                                validator: (val) => val.isEmpty ? "Enter an email" : null,  //null means valid email
+                                decoration: InputDecoration(
+                                    labelText:  'البريد الإلكتروني',
+                                    labelStyle: TextStyle( fontFamily: 'Montserrat',fontWeight: FontWeight.bold, color: Colors.grey),
+                                    focusedBorder: UnderlineInputBorder(
+                                        borderSide: BorderSide(color: Colors.lightBlueAccent))),
+                                onChanged: (val){setState(() => email=val);},
+                              )
+                          ),
+                          SizedBox(height: 10.0),
+                          Directionality(
+                              textDirection: TextDirection.rtl,
+                              child:TextFormField(
+                                validator: (val) => val.length < 6 ? "enter a password 6+ chars long" : null,  //null means valid password
+                                decoration: InputDecoration(
+                                    labelText: 'كلمة المرور ',
+                                    labelStyle: TextStyle(fontFamily: 'Montserrat',fontWeight: FontWeight.bold,color: Colors.grey),
+                                    focusedBorder: UnderlineInputBorder(
+                                        borderSide: BorderSide(color: Colors.lightBlueAccent))),
+                                obscureText: true,
+                                onChanged: (val){ setState(() => password =val);},
+                              )
+                          ),
+                          SizedBox(height: 10.0),
 
-                                onTap: () async {
-                                  if (_formKey.currentState.validate()){
-                                    dynamic result = await _auth.signInWithEmailAndPassword(email, password);
-                                    if (result == null ){
-                                      setState(() => error= 'البريد الإلكتروني أو كلمة المرور غير صحيحة');
-                                    }
-                                  }
-                                },
-                                child: Center(
-                                  child: Text( 'تسجيل الدخول',
-                                    style: TextStyle(
-                                        color: Colors.white,
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 20.0,
-                                        fontFamily: 'Montserrat'),
-                                  ),
-                                ),
-                              ),
-                            )),
-                        SizedBox(height: 20.0),
-                        Container(
-                            height: 40.0,
-                            child: Material(
-                              borderRadius: BorderRadius.circular(20.0),
-                              shadowColor: Colors.lightBlueAccent,
-                              color: Colors.green[300],
-                              elevation: 7.0,
-                              child: GestureDetector(
+                          Container(
+                            padding: EdgeInsets.fromLTRB(0.0, 00.0, 200.0, 50.0),
+                            child: GestureDetector(
+                                child: Text('نسيت كلمة المرور ؟', style:
+                                TextStyle(color: Colors.grey[600], fontSize: 15.0, fontFamily: "Montserrat"),),
                                 onTap: () {
                                   Navigator.push(context, new MaterialPageRoute(
-                                builder: (context) => SignUp_SP()
-                            ));
-                                },
-                                child: Center(
-                                  child: Text( 'تسجيل مستخدم جديد',
-                                    style: TextStyle(
-                                        color: Colors.white,
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 20.0,
-                                        fontFamily: 'Montserrat'),
+                                      builder: (context) => ForgetPassword()
+                                  ));
+                                }
+
+                            ),
+                          ),
+
+                          Container(
+                              height: 40.0,
+                              child: Material(
+                                borderRadius: BorderRadius.circular(20.0),
+                                shadowColor: Colors.lightBlueAccent,
+                                color: Colors.green[300],
+                                elevation: 7.0,
+                                child: GestureDetector(
+
+                                  //اذا دخل ايميل وباسورد مو موجودات مايطلع له مسج !!
+
+                                  onTap: () async {
+                                    if (_formKey.currentState.validate()){
+                                      dynamic result = await _auth.signInWithEmailAndPassword(email, password);
+                                      if (result == null ){
+                                        setState(() => error= 'البريد الإلكتروني أو كلمة المرور غير صحيحة');
+                                      }
+                                    }
+                                  },
+                                  child: Center(
+                                    child: Text( 'تسجيل الدخول',
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 20.0,
+                                          fontFamily: 'Montserrat'),
+                                    ),
                                   ),
                                 ),
-                              ),
-                            )),
+                              )),
+                          SizedBox(height: 20.0),
+                          Container(
+                              height: 40.0,
+                              child: Material(
+                                borderRadius: BorderRadius.circular(20.0),
+                                shadowColor: Colors.lightBlueAccent,
+                                color: Colors.green[300],
+                                elevation: 7.0,
+                                child: GestureDetector(
+                                  onTap: () {
+                                    Navigator.push(context, new MaterialPageRoute(
+                                        builder: (context) => SignUp_SP()
+                                    ));
+                                  },
+                                  child: Center(
+                                    child: Text( 'تسجيل مستخدم جديد',
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 20.0,
+                                          fontFamily: 'Montserrat'),
+                                    ),
+                                  ),
+                                ),
+                              )),
 
-                      ],
-                    )),
-              ])
-          )
+                        ],
+                      )),
+                ])
+            )
         )
 
 
