734f99610292e711f94e81301ff726f07153e481
gpgirls20 <gpgirls20@gmail.com>
2020-03-22 20:55:12 +0300

    shahad commit Sun 8:55 pm

--- lib/screens/availableSP.dart
+++ lib/screens/availableSP.dart
@@ -16,27 +16,21 @@ class availableSP extends StatefulWidget {
 //----------------------------Available SP----------------------------------------
 
 List<myData> allData = [];
-List<myData> SPData = [];
+List<myData> sortedData = [];
 List<myData> dummyList = List<myData>();
 
 class myData {
-  String email,name, phoneNumber, service,subService,qualifications;
-  var uid, price,latitude,longitude;
+  String email,name, phoneNumber, service,qualifications,startTime,endTime;
+  double raringPrice,ratingCooperation,ratingTime,ratingWork,ratingAvg,price;
+  int ratingCounter;
+  var uid,latitude,longitude;
+  bool available;
   var loc='';
 
-  myData(this.email,this.name, this.phoneNumber, this.service, this.uid,this.price,this.qualifications, this.latitude,this.longitude);
+  myData(this.email,this.name, this.phoneNumber, this.service, this.uid,this.price,this.qualifications, this.latitude,this.longitude,
+this.startTime,this.endTime,this.raringPrice,this.ratingAvg,this.ratingCooperation,this.ratingCounter,this.ratingTime,this.ratingWork,this.available);
 }
 
-//---------------------------Customer Object-----------------------------------------
-
-List<Cust_myData> Custs = [];
-Cust_myData ct ;
-
-class Cust_myData {
-  var uid,latitude,longitude;
-
-  Cust_myData(this.uid, this.latitude, this.longitude);
-}
 
 //--------------------------------------------------------------------
 
@@ -49,10 +43,12 @@ class _availableSPState extends State<availableSP> {
 
   _availableSPState(dynamic u, String cat){
     uid=u;
-    print('availableSP: $uid');
+    //print('availableSP: $uid');
     c=cat;
-    print("Test: $c");
-    SPData.clear();
+   print("Test: $c");
+    //SPData.clear();
+    allData.clear();
+
   }
 
   static const List<String> longItems = const [
@@ -61,74 +57,54 @@ class _availableSPState extends State<availableSP> {
     'المسافة الأقرب أولًا',
   ];
 
-  Future getData() async {
-    //print("===================================getData===================================");
-     await FirebaseDatabase.instance.reference().child('Service Provider').once().then((DataSnapshot snap) async {
-      var keys = snap.value.keys;
-      var data = snap.value;
 
-      allData.clear();
-      myData d;
-      for (var key in keys) {
-        d = new myData(
-          data[key]['email'],
-          data[key]['name'],
-          data[key]['phoneNumber'],
-          data[key]['service'],
-          data[key]['uid'],
-          data[key]['price'],
-          data[key]['qualifications'],
-          data[key]['latitude'],
-          data[key]['longitude'],
-        );
-         allData.add(d);
-      }
-      SPData.clear();
-      for (var i = 0; i < allData.length; i++) {
-        if(allData[i].service == c){
-          SPData.add(allData[i]);
-          print(c);
-          print(allData[i].name);
-        }
-      }
-    });
+  var dbReferenceSP;
+  var _firebaseRefSP =FirebaseDatabase.instance.reference();
+  var dbReferenceCust;
+  var _firebaseRefCust =FirebaseDatabase.instance.reference();
 
-    await FirebaseDatabase.instance.reference().child('Customer').once().then((DataSnapshot snap) async {
-       var keys = snap.value.keys;
-       var data = snap.value;
+  String Custuid;
+  var Custlatitude , Custlongitude;
 
-       Custs.clear();
-       Cust_myData d;
-       for (var key in keys) {
-         d = new Cust_myData(
-           data[key]['uid'],
-           data[key]['latitude'],
-           data[key]['longitude'],
-         );
-         Custs.add(d);
-       }
 
-       ct=null;
-       for (var i = 0; i < Custs.length; i++) {
-         if(Custs[i].uid == uid){
-           ct = Custs[i];
-         }
-       }
-     });
 
-    for(var i=0; i< SPData.length; i++ ){
-      double ddd = await Geolocator().distanceBetween(ct.latitude, ct.longitude, SPData[i].latitude, SPData[i].longitude)/1000;
-      SPData[i].loc=ddd.toStringAsFixed(2);
-      //print(SPData[i].loc);
+  @override
+  initState(){
+    super.initState();
+    dbReferenceSP=_firebaseRefSP.child('Service Provider');
+    dbReferenceCust=_firebaseRefCust.child('Customer').orderByChild('uid').equalTo(uid);
+  }
+
+
+
+  Future Sort() async {
+
+    for(var i=0; i< allData.length; i++ ) {
+      double ddd = await Geolocator().distanceBetween(Custlatitude, Custlongitude, allData[i].latitude, allData[i].longitude)/1000;
+      allData[i].loc=ddd.toStringAsFixed(2);
     }
 
-    if(sortBy=='distance')
-     sortByDistance();
-     if(sortBy=='price')
-       sortByPrice();
-
-     return SPData;
+     return allData;
   }
+
+  void sortByDistance(){
+    dummyList.clear();
+    dummyList.addAll(allData);
+    dummyList.sort((a, b) => double.parse(a.loc).compareTo(double.parse(b.loc)));
+
+      allData.clear();
+      allData.addAll(dummyList);
+  }
+
+  void sortByPrice(){
+    dummyList.clear();
+    dummyList.addAll(allData);
+    dummyList.sort((a, b) => a.price.compareTo(b.price));
+
+      allData.clear();
+      allData.addAll(dummyList);
+  }
+
 
   Widget _buildList() {
     return Container(
@@ -137,9 +113,7 @@ class _availableSPState extends State<availableSP> {
           SizedBox(height: 20.0),
           Row(
             children: <Widget>[
-              Padding(
-                padding: EdgeInsets.all(10.0),
-              ),
+              Padding(padding: EdgeInsets.all(10.0), ),
               DropdownButton<String>(
                 items: longItems.map<DropdownMenuItem<String>>((String text) {
                   return DropdownMenuItem<String>(
@@ -158,17 +132,14 @@ class _availableSPState extends State<availableSP> {
 
                 value: longSpinnerValue,
                 hint: new Text('  ...تـرتيـب بـحسب  '),
-
                 onChanged: (String text) {
                   setState(() {
                     longSpinnerValue = text;
                     print(text);
                     if(text=='السعر الأقل أولًا')
                       sortBy='price';
-                      //sortByPrice();
                     if(text=='المسافة الأقرب أولًا')
                       sortBy='distance';
-                      //sortByDistance();
                   });
                 },
 
@@ -179,12 +150,13 @@ class _availableSPState extends State<availableSP> {
             ],
           ),
           new Expanded(
-            child: ListView.builder(
-                itemCount: SPData.length,
-                itemBuilder: (BuildContext ctxt, int index) {
-                  return _buildListItem(SPData[index]);
-                }
-            ),
+            child:ListView.builder(
+                    itemCount: allData.length,
+                    itemBuilder: (BuildContext ctxt, int index) {
+                      return _buildListItem(allData[index]);
+                    }
+
+            )
           ),
         ],
       ),
@@ -272,27 +244,6 @@ class _availableSPState extends State<availableSP> {
     );
   }
 
-  void sortByDistance(){
-    dummyList.clear();
-    dummyList.addAll(SPData);
-    dummyList.sort((a, b) => a.loc.compareTo(b.loc));
-
-    setState(() {
-      SPData.clear();
-      SPData.addAll(dummyList);
-    });
-  }
-
-  void sortByPrice(){
-    dummyList.clear();
-    dummyList.addAll(SPData);
-    dummyList.sort((a, b) => a.price.compareTo(b.price));
-
-    setState(() {
-      SPData.clear();
-      SPData.addAll(dummyList);
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -317,16 +268,76 @@ class _availableSPState extends State<availableSP> {
       ),
 
       body: Container(
-        child: FutureBuilder(
-          future: getData(),
-          builder: (BuildContext context,AsyncSnapshot snapshot){
-            if(!snapshot.hasData)
-              return Container(child: Center(child: Text("Loading.."),));
-            else
-              return Container(
-                child: _buildList(),
-              );
-          },),
+        child: StreamBuilder(
+          stream: dbReferenceCust.onValue,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting){return Center(child: CircularProgressIndicator(),);}
+            Map custData = snapshot.data.snapshot.value;
+            List Custitem = [];
+            custData.forEach((index, data) => Custitem.add({"key": index, ...data}));
+            Custuid = Custitem[0]['name'];
+            Custlatitude = Custitem[0]['latitude'];
+            Custlongitude = Custitem[0]['longitude'];
+
+            return StreamBuilder(
+              stream: dbReferenceSP.onValue,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting){return Center(child: CircularProgressIndicator(),);}
+                Map SPData = snapshot.data.snapshot.value;
+                var keys = snapshot.data.snapshot.value.keys;
+
+                allData.clear();
+                myData d;
+                for (var key in keys) {
+                  d = new myData(
+                    SPData[key]['email'],
+                    SPData[key]['name'],
+                    SPData[key]['phoneNumber'],
+                    SPData[key]['service'],
+                    SPData[key]['uid'],
+                    SPData[key]['price'].toDouble(),
+                    SPData[key]['qualifications'],
+                    SPData[key]['latitude'],
+                    SPData[key]['longitude'],
+                    SPData[key]['startTime'],
+                    SPData[key]['endTime'],
+                    SPData[key]['raringPrice'],
+                    SPData[key]['ratingAvg'],
+                    SPData[key]['ratingCooperation'],
+                    SPData[key]['ratingCounter'],
+                    SPData[key]['ratingTime'],
+                    SPData[key]['ratingWork'],
+                    SPData[key]['available'],
+                  );
+                  allData.add(d);
+                }
+
+                return Container(
+                  child: FutureBuilder(
+                    future: Sort(),
+                    builder: (context,snapshot){
+                      if(snapshot.connectionState == ConnectionState.waiting){return Center(child: CircularProgressIndicator(),);}
+
+                      sortedData.clear();
+                      for(var i=0 ; i < allData.length ; i++){
+                        if(allData[i].service == c)
+                          sortedData.add(allData[i]);
+                      }
+                        allData.clear();
+                        allData.addAll(sortedData);
+
+                      if(sortBy=='distance')
+                        sortByDistance();
+                      if(sortBy=='price')
+                        sortByPrice();
+                      return Container(child: _buildList());
+                    },
+                  ),
+                );
+              },
+            );
+          },
+        )
       )
     );
   }
